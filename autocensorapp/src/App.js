87cea94f7849136291eaccae3d24b06cbac88f03
RexGm <serkaanaltundag@gmail.com>
2025-08-03 23:47:46 +0300

    ui demo added

---
+++ autocensorapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🚀 This page will be active soon!</h1>
+        <p>We're working hard to bring you an amazing experience.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
